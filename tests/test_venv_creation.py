468fbd55ae568abdda5e2e78fa90605b5cff1c1f
mriswithe <1725647+mriswithe@users.noreply.github.com>
2023-08-05 00:30:08 -0500

    exe_version passes a quick dirty test on our executable

--- tests/test_venv_creation.py
+++ tests/test_venv_creation.py
@@ -4,3 +4,9 @@ def test_jdk_element_matches(jdk_element, test_venv):
 
 def test_misc_element_matches(misc_element, test_venv):
     assert test_venv._build_misc_element() == misc_element
+
+
+def test_parses_our_version_number(test_venv):
+    import sys
+
+    assert test_venv.exe_version == sys.version_info[:3]
