c9c6f0caef62880641e3de5809ec819d2fdfebaf
mriswithe <1725647+mriswithe@users.noreply.github.com>
2023-08-06 22:44:12 -0500

    Add platform specific directories to PyCharm paths
    This commit adds new fields and methods to the platform_paths.py file to include specific directories for PyCharm on different platforms (Windows, Linux, Mac). File locations for configurations, systems, plugins, and logs are provided as per PyCharm's documentation. This will support better cross-platform compatibility and application management.

--- hatch_pycharm/_pycharm/platform_paths.py
+++ hatch_pycharm/_pycharm/platform_paths.py
@@ -1,7 +1,7 @@
 import os
 from functools import wraps
 from pathlib import Path
-from typing import Iterable
+from typing import Iterable, NamedTuple
 
 from hatch.utils.platform import get_platform_name
 
@@ -17,8 +17,31 @@ search_paths = {
     # "macos": (),
 }
 
-
 exe_names = {"windows": "pycharm64.exe", "linux": "pycharm.sh", "macos": "pycharm"}
+config_dirs = {
+    "windows": r"%APPDATA%\JetBrains\{product}{version}",
+    "linux": "~/.config/JetBrains/{product}{version}",
+    "macos": "~/Library/Application Support/JetBrains/{product}{version}",
+}
+system_dirs = {
+    "windows": r"%LOCALAPPDATA%\JetBrains\{product}{version}",
+    "linux": "~/.cache/JetBrains/{product}{version}",
+    "macos": "~/Library/Caches/JetBrains/{product}{version}",
+}
+# From PyCharm docs
+# If you installed PyCharm via the Toolbox App, the plugins directory will be located in the installation directory.
+# To find the installation directory, open the settings of the IDE instance in the Toolbox App, expand Configuration
+# and look for the Install location field. There should be a directory that ends with .plugins.
+plugins_dirs = {
+    "windows": r"%APPDATA%\JetBrains\{product}{version}\plugins",
+    "linux": "~/.local/share/JetBrains/{product}{version}",
+    "macos": "~/Library/Application Support/JetBrains/{product}{version}/plugins",
+}
+logs_dirs = {
+    "windows": r"%LOCALAPPDATA%\JetBrains\{product}{version}\log",
+    "linux": "~/.cache/JetBrains/{product}{version}/log",
+    "macos": "~/Library/Logs/JetBrains/{product}{version}",
+}
 
 
 def resolve_paths(paths: Iterable[str]) -> Iterable[Path]:
@@ -47,6 +70,26 @@ def platform_search_paths() -> Iterable[Path]:
 @not_supported_logger
 def platform_exe_name() -> str:
     return exe_names[get_platform_name()]
+
+
+@not_supported_logger
+def platform_config_dir() -> str:
+    return config_dirs[get_platform_name()]
+
+
+@not_supported_logger
+def platform_system_dir() -> str:
+    return system_dirs[get_platform_name()]
+
+
+@not_supported_logger
+def platform_plugin_dir() -> str:
+    return plugins_dirs[get_platform_name()]
+
+
+@not_supported_logger
+def platform_log_dir() -> str:
+    return logs_dirs[get_platform_name()]
 
 
 if __name__ == "__main__":
