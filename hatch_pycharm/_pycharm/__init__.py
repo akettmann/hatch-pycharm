13ce0f18852eae8ca1be2caa7bdbcaae449cc2ed
mriswithe <1725647+mriswithe@users.noreply.github.com>
2023-09-09 20:20:43 -0500

    remove all of the code for finding Pycharm paths myself. How about we ask the end user to define the path in settings once?

--- hatch_pycharm/_pycharm/__init__.py
+++ hatch_pycharm/_pycharm/__init__.py
@@ -1,90 +1,15 @@
 """This is where we find PyCharms versions that are available and pick the winner"""
 
 import logging
-import os.path
-import shutil
 from collections.abc import Iterable
-from dataclasses import dataclass
 from functools import wraps
 from itertools import chain
 from pathlib import Path
 from typing import NamedTuple
-from xml.etree.ElementTree import Element
 
-from hatch_pycharm._pycharm.jetbrains import find_executable as jb_find
-from hatch_pycharm._pycharm.platform_paths import (
-    platform_search_paths,
-    platform_exe_name,
-    platform_config_dir,
-    platform_system_dir,
-    platform_plugin_dir,
-    platform_log_dir,
-)
-from hatch_pycharm._pycharm.toolbox import find_executable as tb_find
 from hatch_pycharm._pycharm.types import FMT
 
 log = logging.getLogger(__name__)
-
-
-def locate_pycharm():
-    # Easy first try:
-    pycharm = shutil.which(platform_exe_name())
-    if not pycharm:
-        log.info(f"We didn't find an exe on system path, looking elsewhere")
-        tb_exes = [tb_find(fp) for fp in platform_search_paths()]
-        jb_exes = [jb_find(fp) for fp in platform_search_paths()]
-        exes_found = list(filter(None, tb_exes + jb_exes))
-        exes_found.sort(key=lambda x: x[0], reverse=True)
-        pycharm = exes_found[0][1]
-    return pycharm
-
-
-@dataclass(frozen=True, slots=True)
-class _PycharmSettings:
-    """A Singleton class to hold the detected settings for Pycharm"""
-
-    pycharm_exe: Path
-    config_dir: Path
-    system_dir: Path
-    plugins_dir: Path
-    logs_dir: Path
-
-    @property
-    def jdk_tools_xml(self) -> Path:
-        return self.config_dir / "options/jdk.table.xml"
-
-    @classmethod
-    def resolve_paths(cls, version: str = "2023.2"):
-        product, version = "PyCharm", version
-        return cls(
-            pycharm_exe=locate_pycharm(),
-            config_dir=cls.expand_verify(platform_config_dir(), product, version),
-            system_dir=cls.expand_verify(platform_system_dir(), product, version),
-            plugins_dir=cls.expand_verify(platform_plugin_dir(), product, version),
-            logs_dir=cls.expand_verify(platform_log_dir(), product, version),
-        )
-
-    @staticmethod
-    def expand_verify(path_: str, product: str, version: str):
-        expand_vars = os.path.expandvars(path_)
-        log.debug(f"Expanding environment variables: `%s` -> `%s`", path_, expand_vars)
-        formatted = expand_vars.format(product=product, version=version)
-        log.debug(f"Formatted Product and version: `%s` -> `%s`", expand_vars, formatted)
-        expanded_path = Path(formatted).resolve()
-        if not expanded_path.exists():
-            log.warning("Expanded path `%s` to `%s` but it does not appear to exist!", path_, expanded_path)
-        return expanded_path
-
-    def get_jdk_entry_for_environment(self, unique_id) -> Element:
-        pass
-
-
-settings = _PycharmSettings.resolve_paths()
-pycharm_exe = locate_pycharm()
-app_root = pycharm_exe.parent.parent
-plugins_folder = app_root / "plugins"
-jdk_table_xml = app_root / "options/jdk.table.xml"
-_PYCHARM = str(pycharm_exe)
 
 
 class FileRef(NamedTuple):
@@ -114,28 +39,28 @@ def log_command(f):
 
 
 @log_command
-def make_open_file_command(*files: FileRef | Path) -> Iterable[str]:
+def make_open_file_command(pycharm: Path, *files: FileRef | Path) -> Iterable[str]:
     # Making it a list of FileRef instead of FileRef or paths
     files: list[FileRef] = [p if isinstance(p, FileRef) else FileRef(p) for p in files]
-    cmd = [_PYCHARM]
+    cmd = [str(pycharm)]
     cmd.extend(chain.from_iterable(map(FileRef.command_args, files)))
     return cmd
 
 
 @log_command
-def make_compare_file_command(path1: Path, path2: Path, path3: Path = None) -> Iterable[str]:
+def make_compare_file_command(pycharm: Path, path1: Path, path2: Path, path3: Path = None) -> Iterable[str]:
     """
     Runs Pycharm, asking for a diff between two or three files.
     ref: https://www.jetbrains.com/help/pycharm/command-line-differences-viewer.html
     """
-    cmd = [_PYCHARM, "diff", str(path1.absolute()), str(path2.absolute())]
+    cmd = [pycharm, "diff", str(path1.absolute()), str(path2.absolute())]
     if path3:
         cmd.append(str(path3))
     return cmd
 
 
 @log_command
-def make_merge_file_command(path1: Path, path2: Path, output: Path, base: Path = None) -> Iterable[str]:
+def make_merge_file_command(pycharm: Path, path1: Path, path2: Path, output: Path, base: Path = None) -> Iterable[str]:
     """
     Runs PyCharm, asking for a merge between two or three files.
     ref: https://www.jetbrains.com/help/pycharm/command-line-merge-tool.html
@@ -145,7 +70,7 @@ def make_merge_file_command(path1: Path, path2: Path, output: Path, base: Path =
     contents of the output file as the common origin. In this case, if the
     output is an empty file, this essentially becomes a two-way merge.
     """
-    cmd = [_PYCHARM, "merge", str(path1), str(path2)]
+    cmd = [pycharm, "merge", str(path1), str(path2)]
     if base:
         # Base is optional, output is the base if it has content
         cmd.append(str(base))
@@ -155,6 +80,7 @@ def make_merge_file_command(path1: Path, path2: Path, output: Path, base: Path =
 
 @log_command
 def make_format_files_command(
+    pycharm: Path,
     *files: Path,
     masks: Iterable[str] = (),
     recursive: bool = False,
@@ -171,7 +97,7 @@ def make_format_files_command(
     :return:
     """
     cmd = [
-        _PYCHARM,
+        pycharm,
         "format",
     ]
     # Using extend to keep each command on one line where possible
@@ -197,6 +123,7 @@ def make_format_files_command(
 
 @log_command
 def make_code_inspections_command(
+    pycharm: Path,
     project: Path,
     inspection_profile: Path,
     output: Path,
@@ -212,7 +139,7 @@ def make_code_inspections_command(
 
     :return:
     """
-    cmd = [_PYCHARM, "inspect", str(project), str(inspection_profile), str(output)]
+    cmd = [pycharm, "inspect", str(project), str(inspection_profile), str(output)]
     if changes:
         cmd.append("-changes")
     if subdirectory:
@@ -224,7 +151,7 @@ def make_code_inspections_command(
 
 
 @log_command
-def make_install_plugins_command(*plugins: str) -> Iterable[str]:
+def make_install_plugins_command(pycharm: Path, *plugins: str) -> Iterable[str]:
     """
     Runs PyCharm, asking installation of plugins either by plugin-id or repository-url
     ref: https://www.jetbrains.com/help/pycharm/install-plugins-from-the-command-line.html
@@ -235,11 +162,6 @@ def make_install_plugins_command(*plugins: str) -> Iterable[str]:
 
     :return:
     """
-    cmd = [_PYCHARM, "installPlugins"]
+    cmd = [pycharm, "installPlugins"]
     cmd.extend(map(str, plugins))
     return cmd
-
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(name)s [%(levelname)s]:%(message)s")
-    locate_pycharm()
